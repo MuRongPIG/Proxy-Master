c256308007b846db3914abf7312b271eda16311a
lightworld689 <107845122+lightworld689@users.noreply.github.com>
2023-08-29 16:03:57 +0800

    Add version check and little change

--- checkproxy.py
+++ checkproxy.py
@@ -2,33 +2,38 @@ import requests
 import threading
 import os 
 import time
+import platform
+
 def test(type):
-    if type in ['http','https']:
-        with open("http.txt","r") as f:
-            data=f.read().split("\n")
+    if type in ['http', 'https']:
+        with open("http.txt", "r") as f:
+            data = f.read().split("\n")
     else:
-        with open("{}.txt".format(type),"r") as f:
-            data=f.read().split("\n")
-    print('> {} {} proxies will be check'.format(len(data),type))
+        with open("{}.txt".format(type), "r") as f:
+            data = f.read().split("\n")
+    print('> {} {} proxies will be checked'.format(len(data), type))
     
     try:
-        os.system('rm {}_checked.txt'.format(type))
+        if platform.system() == 'Windows':
+            os.system('del {}_checked.txt'.format(type))
+        else:  # Assuming it's Linux
+            os.system('rm {}_checked.txt'.format(type))
     except:
         pass
     
     def process(i):
         try:
-            requests.get("https://icanhazip.com/",proxies={"https":f"{type}://{i}"},timeout=30,)
+            requests.get("https://icanhazip.com/", proxies={type: f"{type}://{i}"}, timeout=30,)
         except:
             pass
         else:
-            with open("{}_checked.txt".format(type),"a+") as f:
-                f.write(i+"\n")
+            with open("{}_checked.txt".format(type), "a+") as f:
+                f.write(i + "\n")
 
     for i in data:
-        threading.Thread(target=process,args=(i,)).start()
+        threading.Thread(target=process, args=(i,)).start()
 
-    while threading.active_count() >1:
+    while threading.active_count() > 1:
         time.sleep(1)
     
 if __name__ == '__main__':
@@ -36,4 +41,3 @@ if __name__ == '__main__':
     test('socks5')
     test('http')
     test('https')
-    
